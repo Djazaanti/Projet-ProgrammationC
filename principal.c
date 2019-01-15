0dc468777d43f5ec401d1fa9c94511eb0c41baef
Djazaanti <43999628+Djazaanti@users.noreply.github.com>
2019-01-15 23:10:40 +0100

    main

---
+++ principal.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include fonctions.h;
+#define MAX
+int i;
+
+int main(){
+
+ChoixTableau();
+tablaux();
+AfficheJeu();
+while(i<(l+c)-Nbmines){
+   Jouer();
+i--;
+}
+
+}
+
+
+
+
+
